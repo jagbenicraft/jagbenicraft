28dbcebdb8a3173c166e703401550c6eb4551c48
jagbenicraft <jagbenicraft@gmail.com>
2024-01-23 15:30:08 +0530

    uploading first trial version

--- main.js
+++ main.js
@@ -1,29 +1,116 @@
-let switches = document.getElementsByClassName('switch');
+function openNav() {
+    document.getElementById("jsnavbar").classList.add("toggleclass");
+  };
+  
+  function closeNav() {
+    document.getElementById("jsnavbar").classList.remove("toggleclass");
+  };
 
-let style = localStorage.getItem('style');
+  
+// Split text into words and characters
+const text = new SplitType("#new", { types: "words, chars" });
 
-if (style == null) {
-  setTheme('light');
-} else {
-  setTheme(style);
+console.clear();
+
+gsap.registerPlugin(ScrollTrigger);
+
+const text2 = new SplitType("#new2", { types: "words, chars" });
+
+
+const al = gsap
+  .timeline({
+    scrollTrigger: {
+        
+      trigger: "#sl2Section",
+      start: "top top",
+      end: "+=150%",
+      pin: ".slide2",
+     
+      scrub: 0.75
+     
+    }
+  })
+
+ .to(
+    text2.words,
+    {
+      y:10,
+      textFillColor: "#9A2827",
+      opacity:1,
+      stagger: 0.1
+    },
+    0.1
+  );
+
+
+const tl = gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".section1",
+      start: "top top",
+      end: "+=150%",
+      pin: ".slide1",
+      
+      scrub: 0.75
+     
+    }
+  })
+
+ .to(
+    text.words,
+    {
+        y:3,
+      textFillColor: "#9A2827",
+      opacity:1,
+      stagger: 0.1
+    },
+    0.1
+  );
+
+
+  
+
+
+
+
+const races = document.querySelector(".races");
+console.log(races.offsetWidth)
+
+function getScrollAmount() {
+	let racesWidth = races.scrollWidth;
+	return -(racesWidth - window.innerWidth);
 }
 
-for (let i of switches) {
-  i.addEventListener('click', function () {
-    let theme = this.dataset.theme;
-    setTheme(theme);
-  });
-}
+const tween = gsap.to(races, {
+	x: getScrollAmount,
+	duration: 3,
+	ease: "none",
+});
 
-function setTheme(theme) {
-  if (theme == 'light') {
-    document.getElementById('switcher-id').href = 'light.css';
-  } else if (theme == 'sky') {
-    document.getElementById('switcher-id').href = 'sky.css';
-  } else if (theme == 'purple') {
-    document.getElementById('switcher-id').href = 'purple.css';
-  } else if (theme == 'dark') {
-    document.getElementById('switcher-id').href = 'dark.css';
-  }
-  localStorage.setItem('style', theme);
-}
+
+ScrollTrigger.create({
+	trigger:".slide4",
+	start:"top top",
+	end: () => `+=${getScrollAmount() * -1}`,
+	pin:".slide4",
+	animation:tween,
+	scrub:1,
+	invalidateOnRefresh:true
+	// markers:true
+
+})
+
+
+
+gsap.from(".navbar a", {
+	
+    y: -10,
+    stagger: 0.2
+});
+
+
+gsap.from(".logo-rec", {
+	
+    y: -10,
+    stagger: 0.2
+});
